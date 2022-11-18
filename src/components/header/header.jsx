4a9400512e82a884f7430ebc7a05437212f039e6
Syed Muhammad Akbar <sm.akbar42@gmail.com>
2022-11-18 10:28:25 +0500

    added heading

---
+++ src/components/header/header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./styles.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="left">
+        <h2>Stock Market Analysis</h2>
+      </div>
+      <div className="right"></div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/containers/home/home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./styles.css";
+import Header from "../../components/header/header";
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+};
+
+export default Home;
